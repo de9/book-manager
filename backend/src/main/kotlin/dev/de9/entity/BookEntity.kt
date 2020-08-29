7efd58ed064d97a82929253bda03d7e757b3346a
t.kanai <gomasio1000mg@gmail.com>
2020-08-30 02:04:58 +0900

    [update]BookEntityに日付フォーマットを追加

--- backend/src/main/kotlin/dev/de9/entity/BookEntity.kt
+++ backend/src/main/kotlin/dev/de9/entity/BookEntity.kt
@@ -1,5 +1,6 @@
 package dev.de9.entity
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDate
 
 /**
@@ -11,5 +12,6 @@ data class BookEntity(
         /** タイトル */
         val title: String,
         /** 出版日 */
+        @JsonFormat(pattern = "yyyy-MM-dd")
         val dateOfPublication: LocalDate
 )
