e67dada54ec074d8b2821b5f2756b9f889f6e7f9
t.kanai <gomasio1000mg@gmail.com>
2020-09-01 18:24:11 +0900

    [add]HTTPクライアントのaxiosを追加

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 import router from './router'
 
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
+console.log(process.env.VUE_APP_API_BASE)
+Vue.prototype.$axios = axios.create({ baseURL: process.env.VUE_APP_API_BASE })
 
 new Vue({
   router,
