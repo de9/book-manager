2a2a4d69b8887541ef6f1ca34c1162416db1880c
t.kanai <gomasio1000mg@gmail.com>
2020-09-01 19:23:05 +0900

    [add]書籍検索ページを追加

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '@/views/HomeView'
+import BooksView from '@/views/BooksView'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'HomeView',
     component: HomeView
+  },
+  {
+    path: '/books',
+    name: 'BooksView',
+    component: BooksView
   }
 ]
 
---
+++ frontend/src/views/BooksView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <label>
+      <input type="text" placeholder="書籍タイトルを入力" v-model="title">
+      <button v-on:click="getBooks">検索</button>
+    </label>
+    <table>
+      <thead>
+      <tr>
+        <th>タイトル</th>
+        <th>出版日</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="book in books" :key="book.id">
+        <td>{{ !book.title ? '（タイトル未定）' : book.title }}</td>
+        <td>{{ book.dateOfPublication }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BooksView',
+  data() {
+    return {
+      title: null,
+      books: []
+    }
+  },
+  mounted() {
+    this.getBooks()
+  },
+  methods: {
+    async getBooks() {
+      let response = await this.$axios.get('/books', {params: {title: this.title}})
+      this.books = response.data
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
